327b974007bf6afeb44122e8b1b84441f7209852
Lauraquat <lauraquat@gmail.com>
2023-02-28 15:22:14 +0100

    possibilité de scanner un nouveau qr code (en sortant de la page et en y retournant)

--- src/pages/Scan.tsx
+++ src/pages/Scan.tsx
@@ -1,5 +1,5 @@
-import { IonPage } from '@ionic/react';
-import { useEffect, useState } from 'react';
+import { IonPage, useIonViewDidEnter } from '@ionic/react';
+import { useState } from 'react';
 
 import './style.css';
 import { BarcodeScanner } from '@capacitor-community/barcode-scanner';
@@ -9,24 +9,25 @@ const Scan: React.FC = () => {
 
   const [qrCode, setQrCode] = useState('');
   
-  useEffect(() => {
-    async function startScan() {
-      // Vérifie la permission de la caméra sur le mobile
-      await BarcodeScanner.checkPermission({ force: true });
-      
-      // Affiche un background transparent
-      BarcodeScanner.hideBackground();
-      
-      const result = await BarcodeScanner.startScan(); // scanne et attend le résultat
-      
-      // Renvoie le résultat s'il existe (sinon renvoie une erreur)
-      if (result.hasContent) {
-        setQrCode(result.content ?? 'Erreur de lecture du QRcode');
-        console.log(result.content); //TODO vérifier que le contenu correspond à ce qu'on a généré en base lors de la réservation
-      }
-    };
+  async function startScan() {
+    // Vérifie la permission de la caméra sur le mobile
+    await BarcodeScanner.checkPermission({ force: true });
+    
+    // Affiche un background transparent
+    BarcodeScanner.hideBackground();
+    
+    const result = await BarcodeScanner.startScan(); // scanne et attend le résultat
+    
+    // Renvoie le résultat s'il existe (sinon renvoie une erreur)
+    if (result.hasContent) {
+      setQrCode(result.content ?? 'Erreur de lecture du QRcode');
+      console.log(result.content); //TODO vérifier que le contenu correspond à ce qu'on a généré en base lors de la réservation
+    }
+  };
+
+  useIonViewDidEnter(() => {
     startScan();
-  }, []);
+  });
   
   return (
     //Affiche le lecteur de QRcode
