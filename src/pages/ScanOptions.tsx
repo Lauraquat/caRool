0172cfbe24a2c04bfc65883e2efb360e7087c52e
Léa SAGNIER <l.sagnier@sumit.fr>
2023-03-24 16:56:47 +0100

    query resa

--- src/pages/ScanOptions.tsx
+++ src/pages/ScanOptions.tsx
@@ -1,14 +1,24 @@
 import { IonButton, IonPage, IonContent, IonHeader } from "@ionic/react";
 import { useHistory, useLocation } from "react-router-dom";
 import { dataReservations } from "../dataBdd";
+import React, { useEffect, useState } from "react";
 
 import "./style.css";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { db } from "../firebaseConfig";
 
 const ScanOptions: React.FC = ({route}:any) => {
     //si trouve pas route l'ajouter quelque pars dans app.tsx
     const navigate = useHistory();
     const location = useLocation<dataReservations>(); 
     const hashResa = location.state?.hashResa;
+    const [rendu, setRendu] = useState(false);
+
+    const retourVelo = query(collection(db, "reservation"), where("hashResa", "==", hashResa));
+    console.log(retourVelo);
+    // getDocs(retourVelo).then((querySnapshot) => {
+    //   setRendu();
+    // });
 
   return (
     //Page de redirection après validation d'un QR code valide
@@ -30,7 +40,7 @@ const ScanOptions: React.FC = ({route}:any) => {
           navigate.push('/retourVelo');
         }}
       >
-        Je rend mon vélo
+        Je rends mon vélo
       </IonButton>
 
       <IonButton
