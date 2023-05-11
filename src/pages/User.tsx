08761746ff57cf020304a2b04b3c2f397a047b0b
Léa SAGNIER <l.sagnier@sumit.fr>
2023-05-11 10:55:18 +0200

    style front user

--- src/pages/User.tsx
+++ src/pages/User.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./style.css";
-import { IonButton, IonPage, IonHeader, IonContent,IonToolbar } from "@ionic/react";
+import { IonButton, IonPage, IonHeader, IonContent,IonToolbar , IonList} from "@ionic/react";
 import { useCurrentUser } from "../hooks/UserHook";
 import { auth } from "../firebaseConfig";
 import { useHistory } from "react-router-dom";
@@ -26,11 +26,17 @@ return(
     <IonPage>
         <IonHeader>
             <IonToolbar>
+                User page
             </IonToolbar>
         </IonHeader>
         <IonContent>
+            <section className="page-message">
+            <IonList>
             <IonButton href="./mesresa">Voir mes reservations</IonButton>
             <IonButton onClick={logOut}>LogOut</IonButton>
+            </IonList>
+            </section>
+
         </IonContent>
     </IonPage>
 )
